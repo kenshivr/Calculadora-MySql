d72597bd987dbacd2549cc881d34ddb49bc21d5d
Kenshi <Aaladar@proton.me>
2024-01-21 18:00:36 -0600

    Se elimina el archivo config

--- server/config.js
+++
@@ -1,7 +0,0 @@
-// Contraseñas para ingresar a la BD
-module.exports = {
-    USER: process.env.USER || 'root',
-    HOST: process.env.HOST || 'localhost',
-    PASSWORD: process.env.PASSWORD || '',
-    NAME: process.env.NAME || 'calculadora'
-}
--- server/index.js
+++ server/index.js
@@ -1,60 +1,62 @@
 const express = require("express"); // Es necesario primero requerir a expres antes de llamarlo 
 const app = express(); // en esta constante
 const cors = require("cors");
-const mysql = require("mysql");
+// const mysql = require("mysql");
+const { Pool } = require("pg");
 const config = require("./config.js");
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host: config.HOST,
+// const db = mysql.createConnection({
+//     host: config.HOST,
+//     user: config.USER,
+//     password: config.PASSWORD,
+//     database: config.NAME
+// });
+
+const pool = new Pool({
     user: config.USER,
+    host: config.HOST,
+    database: config.NAME,
     password: config.PASSWORD,
-    database: config.NAME
+    port: config.PORT
 });
 
-app.post("/post", (req,res) => {
+// app.post("/post", (req,res) => {
+//     const resultado = req.body.resultado;
+
+//     db.query('INSERT INTO operaciones(operacion) VALUES (?)', [resultado],
+//     (err,result) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.send("Registro exitoso");
+//             console.log("Registro exitoso");
+//         }
+//     }
+//     );
+
+// });
+
+app.post("/post", (req, res) => {
     const resultado = req.body.resultado;
+  
+    // Usamos $1 como marcador de posición para evitar SQL injection
+    pool.query("INSERT INTO operaciones(operacion) VALUES ($1)", [resultado], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error en el servidor");
+      } else {
+        res.send("Registro exitoso");
+        console.log("Registro exitoso");
+      }
+    });
+  });
 
-    db.query('INSERT INTO operaciones(operacion) VALUES (?)', [resultado],
-    (err,result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("Registro exitoso");
-            console.log("Registro exitoso");
-        }
-    }
-    );
+// app.get("/get", (req,res) => {
 
-    // db.end();
-});
-
-app.get("/get", (req,res) => {
-
-    db.query('SELECT * FROM operaciones',
-    (err,result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    }
-    );
-
-    // db.end();
-});
-
-// app.put("/update", (req,res) => {
-//     const id = req.body.id;
-//     const nombre = req.body.nombre;
-//     const edad = req.body.edad;
-//     const pais = req.body.pais;
-//     const cargo = req.body.cargo;
-//     const anios = req.body.anios;
-
-//     db.query('UPDATE empleados SET nombre=?, edad=?, pais=?, cargo=?, anios=? WHERE id = ?', [nombre,edad,pais,cargo,anios, id],
+//     db.query('SELECT * FROM operaciones',
 //     (err,result) => {
 //         if (err) {
 //             console.log(err);
@@ -64,25 +66,25 @@ app.get("/get", (req,res) => {
 //     }
 //     );
 
-//     // db.end();
 // });
 
-// app.delete("/delete/:id", (req,res) => {
-//     const id = req.params.id;
+app.get("/get", (req, res) => {
+    // No necesitas cerrar la conexión en el entorno de Pool
+    pool.query("SELECT * FROM operaciones", (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error en el servidor");
+      } else {
+        res.send(result.rows);
+      }
+    });
+  });
 
-//     db.query('DELETE FROM empleados WHERE id = ?', id,
-//     (err,result) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             res.send(result);
-//         }
-//     }
-//     );
-
-//     // db.end();
+// app.listen(3001, () => {
+//     console.log("Corriendo en el puerto 3001");
 // });
 
-app.listen(3001, () => {
-    console.log("Corriendo en el puerto 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
